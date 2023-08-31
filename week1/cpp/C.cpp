60261739c2ee92368208e9d8811fa35409a6835a
Arron2411616261 <15018135093@163.com>
2023-08-31 19:47:55 +0800

    it doesn't work

--- week1/cpp/C.cpp
+++ week1/cpp/C.cpp
@@ -8,11 +8,13 @@ int main()
     int n, m;
     cin >> n >> m;
     vector<int> arr;
-    while (n--)
+    int cnt = 0;
+    while (cnt < n)
     {
         int x;
         cin >> x;
         arr.push_back(x);
+        cnt++;
     }
     sort(arr.begin(), arr.end());
     for (int i = arr.size() - 1; m > 0 && i >= 0; i--, m--)
