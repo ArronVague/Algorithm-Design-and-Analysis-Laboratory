bdba020a512e44b74c30487d7eb4b8b6a14d7c24
arron2411616261 <15018135093@163.com>
2023-09-21 12:28:17 +0800

    commit template of topo_sort.cpp

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 [二分 binary.cpp](template/binary.cpp)
 
 [归并排序 merge_sort.cpp](template/merge_sort.cpp)
+
+[拓扑排序 topo_sort.cpp](template/topo_sort.cpp)
---
+++ template/topo_sort.cpp
@@ -0,0 +1,69 @@
+// https://leetcode.cn/problems/collect-coins-in-a-tree/description/ 收集树中金币
+// 两次拓扑
+
+class Solution
+{
+public:
+    int collectTheCoins(vector<int> &coins, vector<vector<int>> &edges)
+    {
+        int n = coins.size();
+
+        // 模板
+        vector<vector<int>> g(n);
+        vector<int> deg(n);
+        for (auto &e : edges)
+        {
+            int x = e[0], y = e[1];
+            g[x].push_back(y);
+            g[y].push_back(x);
+            // 视情况取入度
+            deg[x]++;
+            deg[y]++;
+        }
+
+        // 模板
+        queue<int> q;
+        for (int i = 0; i < n; ++i)
+        {
+            if (deg[i] == 1 && !coins[i])
+            {
+                q.push(i);
+            }
+        }
+        int ans = n - 1;
+        while (!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            ans--;
+            for (auto &y : g[x])
+            {
+                if (--deg[y] == 1 && !coins[y])
+                {
+                    q.push(y);
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            if (deg[i] == 1 && coins[i])
+            {
+                q.push(i);
+            }
+        }
+        ans -= q.size();
+        while (!q.empty())
+        {
+            int x = q.front();
+            q.pop();
+            for (auto &y : g[x])
+            {
+                if (--deg[y] == 1)
+                {
+                    ans--;
+                }
+            }
+        }
+        return max(ans * 2, 0);
+    }
+};
