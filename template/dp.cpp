dc3262ca69db3b192feb965dff61bdfc177bb6e6
Arron2411616261 <15018135093@163.com>
2023-10-10 19:40:23 +0800

    update dp

--- template/dp.cpp
+++ template/dp.cpp
@@ -1,5 +1,42 @@
+// 背包
+//   0-1背包
+// https://vjudge.net/contest/586721#problem/B 采药
+// https://vjudge.net/contest/586721#problem/C 装箱问题
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, M;
+int t[101];
+int price[101];
+
+int main()
+{
+    cin >> T >> M;
+    for (int i = 1; i <= M; ++i)
+    {
+        cin >> t[i] >> price[i];
+    }
+    vector<vector<int>> dp(M + 1, vector<int>(T + 1, 0));
+    for (int i = 1; i <= M; ++i)
+    {
+        for (int j = 1; j <= T; ++j)
+        {
+            if (j < t[i])
+                dp[i][j] = dp[i - 1][j];
+            else
+            {
+                // 关键：状态转移方程
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - t[i]] + price[i]);
+            }
+        }
+    }
+    cout << dp[M][T] << endl;
+    return 0;
+}
+
 // 线性dp
-// 最大子段和
+//   最大子段和
 // https://vjudge.net/contest/581855#problem/E
 // https://leetcode.cn/problems/maximum-subarray/description/
 
---
+++ week7/README.md
@@ -0,0 +1,5 @@
+# 动态规划
+
+题目
+
+https://vjudge.net/contest/586721#overview
