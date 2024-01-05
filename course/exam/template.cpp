5926d05bed7d27086be182c6d564dad3fac19c58
SmileExpression <15018135093@163.com>
2024-01-05 12:51:46 +0800

    Create template.cpp

---
+++ course/exam/template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <utility>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
